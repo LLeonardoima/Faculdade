428b24ba7a9c57b084275f55b35b5d21e30d5f51
Leonardo <leonardojorgenobredelima935@gmail.com>
2026-04-29 20:37:08 -0300

    conteúdo da faculdade so far

--- lista2/exerc1.java
+++
@@ -1,9 +0,0 @@
-package lista2;
-public class exerc1 {
-    public static void main(String[] args) {
-        System.out.println();
-        for (int i = 0; i <= 10; i++) { //Sempre bom lembrar da estrutura for(variável que vai ser utilizada; condição; consequência após o loop)
-            System.out.println(i);
-        }
-    }
-}
