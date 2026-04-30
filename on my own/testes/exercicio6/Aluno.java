efeb9bb183d0cb637490924fe336500ecc2e7773
Leonardo <leonardojorgenobredelima935@gmail.com>
2026-04-29 21:55:21 -0300

    atividade do dia 28

---
+++ on my own/testes/exercicio6/Aluno.java
@@ -0,0 +1,20 @@
+package exercicio6;
+
+public class Aluno extends Pessoa {
+    private double nota;
+
+    public Aluno(String nome, double nota) {
+        super(nome);
+        this.nota = nota;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", nota: " + nota;
+    }
+
+}
---
+++ on my own/testes/exercicio6/Main.java
@@ -0,0 +1,30 @@
+package exercicio6;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Aluno> alunos = new ArrayList<>();
+
+        alunos.add(new Aluno("Gabriel", 2.4));
+        alunos.add(new Aluno("Beatriz", 7.8));
+        alunos.add(new Aluno("Thiago", 0.5));
+        alunos.add(new Aluno("Mariana", 9.1));
+        alunos.add(new Aluno("Lucas", 4.6));
+        
+        double soma = 0;
+        for (Aluno a : alunos) {
+            soma += a.getNota();
+        }
+
+        double media = soma / alunos.size();
+        System.out.println("Média geral " + media);
+
+        System.out.println("Alunos acima da média geral: ");
+        for (Aluno a : alunos) {
+            if (a.getNota() > media) {
+                System.out.println(a);
+            }
+        }
+    }
+}
