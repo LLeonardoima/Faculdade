e3a6b9da40b37963b33046eef8d17278377b7988
Leonardo <leonardojorgenobredelima935@gmail.com>
2026-05-03 19:04:50 -0300

    atiivdade resolvida

--- on my own/lista2/exerc8.java
+++ on my own/lista2/exerc8.java
@@ -1,18 +1,34 @@
 package lista2;
 
 import java.util.Scanner;
+
 public class exerc8 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double y = 0;
-        double z = 0;
-        for (int i = 0; i < 10; i++) {
+        double y = 0; // guardar o valor da maior variável
+        double z = 0; // guardar o valor da menor variável
+        for (int i = 1; i <= 10; i++) {
             System.out.println("Digite o " + i + "º número");
-            double x = sc.nextDouble();
-            if (x > 0) {
+            double x = sc.nextDouble(); // valor digitado
+            if (x > y) {
+                y = 0;
                 y += x;
+            }
+            if (x < z) {
+                z = 0;
+                z += x;
             }
         }
         sc.close();
+        if (y % 2 == 0) {
+            System.out.println("Número maior " + (int) y);
+        } else {
+            System.out.println("Número maior " + y);
+        }
+        if (z % 2 == 0) {
+            System.out.println("Número menor " + (int) z);
+        } else {
+            System.out.println("Número menor " + z);
+        }
     }
 }
